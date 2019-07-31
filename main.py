a200e90936274a1007fba5e2e4b32618e3982ba7
isamitre <isamitre@gmail.com>
2019-07-30 22:55:22 -0400

    redirected user to edit profile

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+
 import webapp2
 import renderer
 import edit_profile
 import save_profile
 import view_profile
+import social_data
 import helper
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         values = helper.get_template_parameters()
-        self.response.write(renderer.render_template(
+        email = helper.get_user_email()
+        p = social_data.get_user_profile(email)
+        if p:
+            self.response.write(renderer.render_template(
                                     self, "main_page.html", values))
+        else:
+            self.response.write(renderer.render_template(
+                                    self, "edit_profile.html", values))
 
 app = webapp2.WSGIApplication([
     ('/profile-edit', edit_profile.Handler),
