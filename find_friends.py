2eabcdb8fde9003293b7d70043d78917fb0d3370
GabrielGonzalezDiaz <gabrielgonzalezdiaz2001@gmail.com>
2019-07-31 12:24:01 -0400

    ITS ALIVE!

--- find_friends.py
+++ find_friends.py
@@ -1,9 +1,22 @@
 import webapp2
 import helper
 import renderer
+import friends_suggestion
 
 
 class Handler(webapp2.RequestHandler):
-    def post(self):
+    def get(self):
         values = helper.get_template_parameters()
+        emails = friends_suggestion.find_suggestions()
+        first_names = []
+        last_names = []
+        for e in emails:
+            first_names.append(helper.get_user_first_name(e))
+        for e in emails:
+            last_names.append(helper.get_user_last_name(e))
+        prospects = []
+        for i in range(0, len(emails)):
+            prospects.append((first_names[i], last_names[i]))
+
+        values['prospects'] = prospects
         renderer.render_template(self, 'find_friends.html', values)
--- friends_suggestion.py
+++ friends_suggestion.py
@@ -1,19 +1,19 @@
 from google.appengine.api import users
 from user_models import UserProfile
-from user_models import get_user_interest
+import helper
 
 
 def find_suggestions():
 
-    user = users.get_current_user()
-    user_interests = set(get_user_interest(user))
+    user = helper.get_user_email()
+    user_interests = set(helper.get_user_interest(user))
     suggestions = {}
     for prospect in UserProfile.query():
         interests_in_common = 0
-        for interest in get_user_interest(prospect):
+        for interest in helper.get_user_interest(prospect.email):
             if interest in user_interests:
                 interests_in_common += 1
         suggestions[prospect.email] = interests_in_common
     prospect_list = suggestions.keys()
-    return prospect_list.sort(key=lambda name: suggestions[name])[::-1]
-
+    prospect_list.sort(key=lambda name: suggestions[name])
+    return prospect_list[::-1]
